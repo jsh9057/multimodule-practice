37a2956b9b5e5fded3b74de5110c3f38732e7580
jsh9057 <jsh9057@skuniv.ac.kr>
2023-10-26 20:32:36 +0900

    [add] multimodule 객체 공유와 ExceptionHandler
    * multimodule 에서 자바 객체공유 가능
    * 빈을 객체를 공유하기 위해서는 ComponentScane 을 통한 다른 모듈의 빈을 스캔해야 함.
    * 패키지의 경로를 맞추는 방법이 있음
    * `SpringBootApplication`의 `scanBasePackages`를 이용하여 스캔할 경로를 명시하는 방법
    
    * Exception 은 기본적으로 랩핑하여 관리
    * 랩핑한 Exception을 `@RestControllerAdvice` 사용하여 핸들링 할 수 있음.
    * 클라이언트와 메시지를 주고 받을 때는 고정된 메시지 방식으로 주고 받는것이 보편적

---
+++ module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
@@ -0,0 +1,34 @@
+package dev.be.moduleapi.controller;
+
+import dev.be.moduleapi.exception.CustomException;
+import dev.be.moduleapi.service.DemoService;
+import dev.be.modulecommon.enums.CodeEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DemoController {
+
+    private final DemoService demoService;
+
+    @GetMapping("/save")
+    public String saver(){
+        return demoService.save();
+    }
+
+    @GetMapping("/find")
+    public String save(){
+        return demoService.find();
+    }
+
+    @GetMapping("/exception")
+    public String exception(){
+        if (true) {
+            throw new CustomException(CodeEnum.UNKNOWN_ERROR);
+        }
+        return demoService.exception();
+    }
+
+}
